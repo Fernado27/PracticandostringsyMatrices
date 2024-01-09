3c686daeddb02d51d617711383a229a39f2400bb
Fernando27 <fernandobasulto27@gmail.com>
2024-01-09 07:27:40 -0400

    Ejerccio cargar matriz con numero 5

--- src/PracticaArraysVectores.java
+++ src/PracticaArraysVectores.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class PracticaArraysVectores {
     public static void main(String[] args) {
         //Ejercicio con 3 vectores: Temperaturas de Ciudades
-        String ciudades[] = new String[5];
+       /* String ciudades[] = new String[5];
         int[] tempmax = new int[5];
         int[] tempmin = new int[5];
         int tempmintotal = 0;
@@ -31,11 +31,33 @@ public class PracticaArraysVectores {
         }
 
         System.out.println("La temperatura maxima de todas las ciudades es " + tempmaxtotal + " grados");
-        System.out.println("La temperatura minima de todas las ciudades es " + tempmintotal + " grados");
+        System.out.println("La temperatura minima de todas las ciudades es " + tempmintotal + " grados");*/
 
         //lol
         //Para el tercer commit prueba
         //Cuarto commit
+
+        //llevar a cabo un programa que permite cargar completamente con numeros 5 una matriz de 4x5
+
+        int matriz[][] = new int[4][5];
+        Scanner user = new Scanner(System.in);
+        for (int i=0;i<4;i++){
+            for (int j=0;j<5;j++){
+                System.out.println("Ingrese el numero '5'");
+                matriz[i][j] = user.nextInt();
+                while(matriz[i][j]!=5){
+                    System.out.println("No ingreso el numero '5'. Porfavor ingrese el numero 5");
+                    matriz[i][j] = user.nextInt();
+                }
+            }
+        }
+
+        for (int i=0;i<4;i++){
+            for (int j=0;j<5;j++){
+                System.out.println("Fila: " + i + ". Columna: " + j + ". Valor: " + matriz[i][j]);
+            }
+        }
+
 
     }
 
